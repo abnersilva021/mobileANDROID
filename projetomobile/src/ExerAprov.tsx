f5e28ee35b9c182ee29d01fb26dae15d71e0e5e3
abnersilva021 <abnerestudioso021@gmail.com>
2024-05-02 21:57:07 -0300

    Aula02/05/2024
    Começo da atividade TelaLogin , ExerAprov.

---
+++ projetomobile/src/ExerAprov.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import {Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+
+
+
+
+
+const Aprovado = () =>{
+    //states
+    const[nota1, setNota1] = useState(0);
+    const[nota2, setNota2] = useState(0);
+    const[nome, setNome] = useState('');
+
+    const Calcular = () => {
+        let resultado = ''
+        if((nota1 + nota2)/ 2 >=7){
+            
+            resultado = 'parabens voce ' + nome + ' aprovado ' + (nota1 + nota2)/ 2
+    
+        }else{
+            resultado = 'Infelizmente o aluno  ' + nome + ' foi reprovado ' + (nota1 + nota2)/ 2 
+        }
+        Alert.alert(resultado);
+    };
+
+    //return vai ter os inputs
+    return (
+       <>
+       <view style={StyleSheet.container}>
+               
+                <TextInput
+                    style={StyleSheet.input}
+                    placeholder="Digite seu email"
+                />
+                <TextInput
+                    style={StyleSheet.input}
+                    secureTextEntry={true}
+                    placeholder="Digite sua senha"
+                />
+                <TouchableOpacity
+                    style={StyleSheet.botao}
+                    onPress={() => { clicou() }}
+                >
+                    <Text style={StyleSheet.botaoText}>Login</Text>
+                </TouchableOpacity>
+                </view>
+       </>
+    );
+
+
+    //botão chama calcular
+
+    
+}
+
+export default Aprovado;
