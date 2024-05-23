18d21e0f2861782c9b3b773d8a3219601c681e61
abnersilva021 <abnerestudioso021@gmail.com>
2024-05-22 22:13:44 -0300

    aula dia  22/05/2024
    .....

--- projetomobile/App.tsx
+++ projetomobile/App.tsx
@@ -1,33 +1,11 @@
-import React from 'react';
-import {Text, TextInput, View} from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
 
-import HelloWorld from './src/HelloWorld';
-import Gato from './src/Exemplo2Gato';
-import NomePersonalizado from './src/ExemploParametro';
-import Aprovado from './src/ExerAprov';
-import TelaLogin from './src/TelaLogin';
-import Atividade22 from './src/Atividade22';
-
-import MeuComponente from './src/Atividade21';
-
-function App(): React.JSX.Element{
-  return(
-    <Atividade22/>
-    //<MeuComponente titulo='Nome' />
-    //<Tela/>
-    
-  //  <>
-  //    <View style={{backgroundColor: 'green', borderRadius: 15}}>
-
-  //      <HelloWorld/>
-  //      <Gato/>
-  //      <NomePersonalizado nome = 'Abner' sobrenome = {'Silva'}/>
-  //      <Aprovado nome = 'Abner' nota1 = {2} nota2 = {3}/>
-  //    </View>
-  // </>
-
+export default function App() {
+  return (
+    <NavigationContainer>
+       <Text>Oii</Text>
+    </NavigationContainer>
   );
 }
-
-export default App;
-
