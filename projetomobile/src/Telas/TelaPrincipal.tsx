33e4908ef8e9fb905f98c7d64a6e7c0a39f7bc43
abnersilva021 <abnerestudioso021@gmail.com>
2024-05-24 19:52:30 -0300

    aula dia 24/05/2024
    Alterações do dia ListaFlat

--- projetomobile/src/Telas/TelaPrincipal.tsx
+++ projetomobile/src/Telas/TelaPrincipal.tsx
@@ -1,13 +1,35 @@
 import React, {useState} from 'react';
 import { PrincipalProps } from '../navigation/HomeNavigator';
-import { Text, View, StyleSheet, TextInput, Pressable, Image, Alert } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Pressable, Image, Alert, TouchableOpacity } from 'react-native';
 
 const TelaPrincipal = ({navigation, route}: PrincipalProps) =>{
     return(
-        <View style={styles.container}>
-            <Text style={styles.titulo}>Bem vindo</Text>
+        <View >
+            <Text>Bem vindo</Text>
+
+            <TouchableOpacity
+                    onPress={() => { navigation.navigate('ExerAprov') }}
+                >
+                    <Text >Login</Text>
+                </TouchableOpacity>
         </View>
     )
 }
 
+
 export default TelaPrincipal;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'purple'
+    },
+    container_login: {
+        flex: 2,
+        alignItems: 'center'
+    },
+    titulo_caixa_texto:{
+        fontSize: 25,
+        color: 'black'
+    }
+});
